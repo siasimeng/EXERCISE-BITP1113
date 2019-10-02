58330f36b33b3bb95fdc68c48671dafed30bf6d3
siasimeng <55194373+siasimeng@users.noreply.github.com>
2019-10-02 12:39:22 +0800

    Update Exercise 3 Q3.cpp

--- Exercise 3 Q3.cpp
+++ Exercise 3 Q3.cpp
@@ -1,17 +1,20 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main()
+void main()
 {
-	double loan_amount, annual_interest_rate, number_of_years, monthly_payment;
-	cout << "Input loan amount (RM): ";
-	cin  >> loan_amount;
-	cout << "Input annual interest rate(%): ";
-	cin  >> annual_interest_rate;
-	cout << "Input number of years: ";
-	cin  >> number_of_years;
-	monthly_payment = ((loan_amount * (annual_interest_rate / 100) * number_of_years) + loan_amount)/(number_of_years * 12);
+	int loan_amount, years, monthly_payment;
+	double annual_interest_rate;
+
+	cout << "Input loan amount(RM) : ";
+	cin >> loan_amount;
+	cout << "Input annual interest rate(%) : ";
+	cin >> annual_interest_rate;
+	cout << "Input number of years : ";
+	cin >> years;
+	//calculation
+	monthly_payment = loan_amount * (1 + annual_interest_rate / 100 * years) / (years * 12);
+
 	cout << "Monthly payment for this loan is (RM): " << monthly_payment << endl;
 	system("pause");
-	return 0;
 }
