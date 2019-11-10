bfbde0bdb5ca9ec48c0254852656ff7024c64a5a
siasimeng <55194373+siasimeng@users.noreply.github.com>
2019-11-10 17:32:43 +0800

    Update Question 8.cpp

--- Question 8.cpp
+++ Question 8.cpp
@@ -1,61 +1,64 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-void getScore(int ,int ,int ,int,int, bool&);
-void calcAverage(int, int, int, int, int);
-int findLowest(int, int, int, int, int);
+void getScore(int num[6], bool&);
+void calcAverage(int num[6]);
+int findLowest(int num[6]);
 
 int main()
 {
-	int num1, num2, num3, num4, num5;
+	int num[6];
 	bool valid;
 
-	cout << "Enter 5 test score: ";
-	cin >> num1 >> num2 >> num3 >> num4 >> num5;
-	getScore(num1, num2, num3, num4, num5, valid);
+	cout << "Enter 5 test scores : "; 
+	//input 5 test score
+	for (int i = 0; i < 5; i++)
+	{
+		cin >> num[i];
+	}
+
+	getScore(num, valid);
 	if (valid == 0)
 		cout << "Invalid test score." << endl;
-	else
-		calcAverage(num1, num2, num3, num4, num5);
 	
+	else	
+		calcAverage(num);
+
 	system("pause");
 	return 0;
 }
 
-void getScore(int num1, int num2, int num3, int num4, int num5, bool& valid)
+//validate test score is not lower than 0 or higher than 100
+void getScore(int num[6], bool& valid)
 {
-	if (num1 < 0 || num1 > 100)
-		valid = 0;
-	if (num2 < 0 || num2 > 100)
-		valid = 0;
-	if (num3 < 0 || num3 > 100)
-		valid = 0;
-	if (num4 < 0 || num4 > 100)
-		valid = 0;
-	if (num5 < 0 || num5 > 100)
-		valid = 0;
+	for (int i = 0; i < 5; i++)
+	{
+		if (num[i] < 0 || num[i] > 100)
+			valid = 0;
+	}
 }
 
-void calcAverage(int num1, int num2, int num3, int num4, int num5)
+//calculate average of 4 highest test scores
+void calcAverage(int num[6])
 {
-	double averageScore;
-	averageScore = num1 + num2 + num3 + num4 + num5 - findLowest(num1, num2, num3, num4, num5);
-	averageScore /= 4;
-	cout << "Average of test scores is " << averageScore  << endl;
+	double average = 0 - findLowest(num);				//remove lowest test score
+	for (int i = 0; i < 5; i++)
+	{
+		average += num[i];
+	}
+	average /= 4;
+	cout << "The average number of 4 highest test scores is " << average << endl;
 }
 
-int findLowest(int num1, int num2, int num3, int num4, int num5)
+//find the lowest of the 5 test scores
+int findLowest(int num[6])
 {
-	int lowScore = num1;
-
-	if (lowScore > num2)
-		lowScore = num2;
-	else if (lowScore > num3)
-		lowScore = num3;
-	else if (lowScore > num4)
-		lowScore = num4;
-	else if (lowScore > num5)
-		lowScore = num5;
-
-	return lowScore;
+	//store the lowest score in num[5]
+	num[5] = num[0];
+	for (int i = 1; i < 5; i++)
+	{
+		if (num[i] < num[5])
+			num[5] = num[i];
+	}
+	return num[5];
 }
